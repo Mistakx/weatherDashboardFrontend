11b220dcf7eeb439db518156e41f28f233365bb4
Mistakx <ade9@live.com.pt>
2022-03-02 19:25:17 +0000

    Restructured repository folders

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MapMistakx/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MapMistakx from './components/MapMistakx.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MapMistakx
   }
 }
 </script>
